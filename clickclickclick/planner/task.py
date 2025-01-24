d521e7a268c66d34d95c5d15fd03032949198cbb
mkagenius <mkagenius@gmail.com>
2025-01-24 13:29:40 +0530

    add Gradio interface for running the tool easily

--- clickclickclick/planner/task.py
+++ clickclickclick/planner/task.py
@@ -1,19 +1,33 @@
 import re
 import time
 from concurrent.futures import ThreadPoolExecutor, TimeoutError
-from typing import Callable, Any
+from typing import Callable, Any, Generator, List
 
 from . import logger
 from clickclickclick.config import BaseConfig
 from clickclickclick.executor import Executor
 from clickclickclick.finder import BaseFinder
 from . import Planner
+import tempfile
+import base64
 
+def create_tempfile_from_base64(base64_string):
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    tmp.write(base64.b64decode(base64_string))
+    tmp.close()
+    return tmp.name
+
+def save_screenshot(screenshot, is_base64):
+    if is_base64:
+        path = create_tempfile_from_base64(screenshot)
+        return path
+    return screenshot
 
 def execute_task(
     prompt: str, executor: Executor, planner: Planner, finder: BaseFinder, c: BaseConfig
 ) -> bool:
     try:
+
         while True:
             screenshot = executor.screenshot(
                 "Planner took screenshot",
@@ -37,7 +51,7 @@ def execute_task(
 
                 if executed_fn_name == "task_finished":
                     return True
-
+#
                 if finder_output is not None:
                     coordinates = list(map(int, finder_output.split(",")))
                     scaled_coordinates = finder.scale_coordinates(coordinates)
@@ -51,9 +65,64 @@ def execute_task(
                     message = f"The UI bounds of the {ui_element} is {finder_output} and it has been clicked "
                     planner.add_finder_message(message)
 
+                observation = func_args.get("observation", "")
+
     except Exception as e:
         logger.exception("An error occurred during task execution.")
         return False
+        # raise e
+
+def execute_task_with_generator(
+    prompt: str, executor: Executor, planner: Planner, finder: BaseFinder, c: BaseConfig
+) -> Generator[List[str], None, bool]:
+    try:
+        observation = ""
+        while True:
+            screenshot = executor.screenshot(
+                "Planner took screenshot",
+                executor.screenshot_as_base64,
+                executor.screenshot_as_tempfile,
+            )
+            logger.info("Generated screenshot")
+            time.sleep(c.TASK_DELAY)
+
+            if executor.screenshot_as_base64:
+                yield [(create_tempfile_from_base64(screenshot), observation)]
+            else:
+                yield [(screenshot, observation)]
+            llm_responses = planner.llm_response(prompt, screenshot)
+            for func_name, func_args in llm_responses:
+                finder_output = None
+                logger.debug(f"Executing {func_name} with {func_args}")
+                (execution_output, executed_fn_name) = parse_and_execute(
+                    func_name, func_args, executor, planner, finder
+                )
+                if executed_fn_name == "find_element_and_click":
+                    logger.info(f"Executed Finder with output: {execution_output}")
+                    ui_element = func_args.get("prompt", "")
+                    finder_output = execution_output
+
+                if executed_fn_name == "task_finished":
+                    return
+#
+                if finder_output is not None:
+                    coordinates = list(map(int, finder_output.split(",")))
+                    scaled_coordinates = finder.scale_coordinates(coordinates)
+                    executor.click_at_a_point(
+                        (coordinates[0] + coordinates[2]) // 2,
+                        (coordinates[1] + coordinates[3]) // 2,
+                        "Clicking center right away",
+                    )
+                    finder_output = ",".join(map(str, scaled_coordinates))
+
+                    message = f"The UI bounds of the {ui_element} is {finder_output} and it has been clicked "
+                    planner.add_finder_message(message)
+
+                observation = func_args.get("observation", "")
+
+    except Exception as e:
+        logger.exception("An error occurred during task execution.")
+        raise e
 
 
 # TODO: move to utils
--- main.py
+++ main.py
@@ -2,51 +2,12 @@ import click
 import os
 from clickclickclick.config import get_config
 from clickclickclick.planner.task import execute_with_timeout, execute_task
-from clickclickclick.executor.osx import MacExecutor
-from clickclickclick.executor.android import AndroidExecutor
-from clickclickclick.planner.gemini import GeminiPlanner
-from clickclickclick.finder.gemini import GeminiFinder
-from clickclickclick.planner.openai import ChatGPTPlanner
-from clickclickclick.finder.local_ollama import OllamaFinder
-from clickclickclick.finder.openai import OpenAIFinder
-from clickclickclick.planner.local_ollama import OllamaPlanner
-from clickclickclick.finder.mlx import MLXFinder
+from utils import get_executor, get_finder, get_planner
 
 
 @click.group()
 def cli():
     pass
-
-
-def get_executor(platform):
-    if platform.lower() == "osx":
-        return MacExecutor()
-    return AndroidExecutor()
-
-
-def get_planner(planner_model, config, executor):
-    if planner_model.lower() == "openai":
-        executor.screenshot_as_base64 = True
-        return ChatGPTPlanner(config)
-    elif planner_model.lower() == "gemini":
-        executor.screenshot_as_tempfile = True
-        return GeminiPlanner(config)
-    elif planner_model.lower() == "ollama":
-        executor.screenshot_as_tempfile = True
-        return OllamaPlanner(config, executor)
-    raise ValueError(f"Unsupported planner model: {planner_model}")
-
-
-def get_finder(finder_model, config, executor):
-    if finder_model.lower() == "openai":
-        return OpenAIFinder(config, executor)
-    elif finder_model.lower() == "gemini":
-        return GeminiFinder(config, executor)
-    elif finder_model.lower() == "ollama":
-        return OllamaFinder(config, executor)
-    elif finder_model.lower() == "mlx":
-        return MLXFinder(config, executor)
-    raise ValueError(f"Unsupported finder model: {finder_model}")
 
 
 def setup_environment_variables(planner=None, finder=None):
